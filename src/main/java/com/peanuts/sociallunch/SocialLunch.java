6bd4483cfcc7f82d66b3d4e5f473e06298356305
Peter Juhasz <jpeter82@gmail.com>
2017-11-22 16:07:23 +0100

    Delete dead code

--- src/main/java/com/peanuts/sociallunch/SocialLunch.java
+++ src/main/java/com/peanuts/sociallunch/SocialLunch.java
@@ -28,23 +28,5 @@ public class SocialLunch {
 
     }
 
-    /*public static void populateDb (EntityManager em) {
-
-        User newUser = new User("John", "Doe", "john@doe.com", "+3615425425", "mfwkmfkem","",(byte) 1,(byte) 0,null,null);
-        Address newAdress = new Address("Hungary", "Budapest", "1111", "Csocsi u. 10. 1/5", "Home");
-        Place newPlace = new Place(newAdress, 112);
-        Timestamp date = new Timestamp(52534L);
-        Event newEvent = new Event(newUser, newPlace,"cool", date, "Első buli");
-        Event eventTwo = new Event(newUser, newPlace,"cool2", date, "Első buli2");
-
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        em.persist(newAdress);
-        em.persist(newPlace);
-        em.persist(newUser);
-        em.persist(newEvent);
-        em.persist(eventTwo);
-        transaction.commit();
-        System.out.println("Commit done");
-    }*/
+    
 }
