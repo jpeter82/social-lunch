ca0aa8ff13d322530d507d1c52eae4efc090bdf4
Peter Juhasz <jpeter82@gmail.com>
2017-11-23 15:08:16 +0100

    Add save method to Event dao

--- src/main/java/com/peanuts/sociallunch/dao/EventDao.java
+++ src/main/java/com/peanuts/sociallunch/dao/EventDao.java
@@ -1,4 +1,21 @@
 package com.peanuts.sociallunch.dao;
 
+import com.peanuts.sociallunch.model.Event;
+
+import javax.persistence.EntityManager;
+
 public class EventDao {
+
+    private EntityManager entityManager;
+
+    public EventDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(Event event) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(event);
+        entityManager.getTransaction().commit();
+    }
+
 }
