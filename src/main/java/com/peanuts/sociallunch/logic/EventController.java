a4a2efc87b5517f04cb569101363a7dcab1e6af8
Peter Juhasz <jpeter82@gmail.com>
2017-11-23 15:05:31 +0100

    Add EventController

--- src/main/java/com/peanuts/sociallunch/logic/EventController.java
+++ src/main/java/com/peanuts/sociallunch/logic/EventController.java
@@ -1,7 +1,70 @@
 package com.peanuts.sociallunch.logic;
 
+
+import com.peanuts.sociallunch.dao.EventDao;
+import com.peanuts.sociallunch.model.Address;
+import com.peanuts.sociallunch.model.Event;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EventController {
 
+    private EventDao eventDao;
+
+    public EventController(EventDao eventDao) {
+        this.eventDao = eventDao;
+    }
+
     // Handles one event view, create new event
+    public ModelAndView showAddNewForm() {
+        //List<Address> result = eventDao.getAll();
+        Map params = new HashMap<>();
+        //params.put("address", result);
+
+        return new ModelAndView(params,"/add-event-form");
+    }
+
+    public ModelAndView createNewEvent(Request request, Response response) {
+
+        String title = request.queryParams("event-title");
+        Integer capacity = Integer.parseInt(request.queryParams("event-capacity"));
+        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date datetime = new Date();
+
+        try {
+            datetime = dateTimeFormatter.parse(request.queryParams("event-date"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Address address = new Address("Hungary", "Budapest", "1111", "Some street 43. 4/2", "Home");
+        String description = request.queryParams("event-description");
+
+        if (true) {
+            Event newEvent = new Event();
+            newEvent.setTitle(title);
+            newEvent.setCapacity(capacity);
+            newEvent.setDate(datetime);
+            newEvent.setDescription(description);
+
+            eventDao.save(newEvent);
+            response.redirect("/event-created");
+        }
+
+        Map params = new HashMap<>();
+        return new ModelAndView(params,"/add-event-form");
+    }
+
+    public ModelAndView addedEvent() {
+        Map params = new HashMap<>();
+        return new ModelAndView(params,"/event-added");
+    }
 
 }
