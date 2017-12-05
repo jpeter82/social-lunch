606a65b384ea916d71a9c085f1084f1bd2e2c1e5
Gabor Muranyi <gabor.muranyi@gmail.com>
2017-12-05 21:19:48 +0100

    Permit css, images

--- src/main/java/com/peanuts/sociallunch/config/SecurityConfig.java
+++ src/main/java/com/peanuts/sociallunch/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/event/**").permitAll()
+                .antMatchers("/", "/event/**", "/css/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
