ba4851d30a1e991719b8dd5347dc5b8002e608c5
Andras Szabo <andras.l.szabo@gmail.com>
2017-12-07 11:46:55 +0100

    session logout

--- src/main/java/com/peanuts/sociallunch/logic/UserController.java
+++ src/main/java/com/peanuts/sociallunch/logic/UserController.java
@@ -4,13 +4,16 @@ import com.peanuts.sociallunch.model.User;
 import com.peanuts.sociallunch.repository.UserRepository;
 //import com.peanuts.sociallunch.util.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 import java.util.List;
 
@@ -22,6 +25,8 @@ public class UserController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
 
     public void setCryptPass(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
@@ -45,18 +50,26 @@ public class UserController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, Principal principal) {
+    public String login(Model model,
+                        @RequestParam(value = "logout", required = false) String logout) {
         //String name = principal.getName();
         //model.addAttribute("username", name);
+        if (logout != null) {
+            System.out.println("logout done");
+            model.addAttribute("message", "Logged out from JournalDEV successfully.");
+        }
+
+
         return "login";
     }
 
-//    @RequestMapping(value = "/login", method = RequestMethod.POST)
-//    public String loginpost(Model model, Principal principal) {
-//        String name = principal.getName();
-//        model.addAttribute("username", name);
-//        System.out.println("model: " + model.toString());
-//        System.out.println("Ez a name: " + name);
-//        return "home/index";
-//    }
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(Model model, HttpSession session) {
+        session.invalidate();
+        System.out.println("logout done");
+        model.addAttribute("message", "Logged out, see You soon.");
+
+        return "login";
+    }
+
 }
